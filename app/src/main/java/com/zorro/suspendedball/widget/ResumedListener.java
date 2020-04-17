315810e96f3e316a9dd3bf13777dec7c1c98450b
Zorro <zorro.pang@qq.com>
2020-04-17 19:21:57 +0800

    悬浮球

---
+++ app/src/main/java/com/zorro/suspendedball/widget/ResumedListener.java
@@ -0,0 +1,10 @@
+package com.zorro.suspendedball.widget;
+
+/**
+ * Created by yhao on 2017/12/30.
+ * https://github.com/yhaolpz
+ */
+
+public interface ResumedListener {
+    void onResumed();
+}
